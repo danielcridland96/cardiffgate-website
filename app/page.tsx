8b44b6ddc9cb798440dc56c54e6858bb4fa706a7
Daniel Cridland <daniel.cridland@srm.com>
2026-06-29 11:51:20 +0100

    Centre available spaces grid on home page

--- app/page.tsx
+++ app/page.tsx
@@ -128,7 +128,7 @@ export default function Home() {
               View all spaces →
             </Link>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-4xl mx-auto">
             {featuredSpaces.map((space) => (
               <div
                 key={space.name}
